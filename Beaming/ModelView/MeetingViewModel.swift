23daaf6ceab967cdaa4409d4b6fe8cad2aba3318
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2026-07-02 11:05:24 +0700

    feat: add meetingview UI

---
+++ Beaming/ModelView/MeetingViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  MeetingViewModel.swift
+//  Beaming
+//
+//  Created by Christopher Hardy Gunawan on 02/07/26.
+//
+
--- Beaming/View/MeetingView.swift
+++ Beaming/View/MeetingView.swift
@@ -10,8 +10,66 @@ import SwiftUI
 struct MeetingView: View {
     var body: some View {
         VStack {
-            Text("Ini Meeting")
+            HStack {
+                Text("Chris's Room")
+                    .font(.title)
+                    .bold()
+                
+                Spacer()
+                
+                Text("6/8")
+                    .font(.title3)
+            }
+            
+            Divider()
+            
+            List {
+                Text("Christopher Hardy Gunawan")
+                Text("Axel Nino Nakata")
+                Text("Muhammad Fadhil Abidin")
+                Text("Muhammad Nafriel Ramadhan")
+                Text("Ananta Ghaisani")
+                Text("Ulfa Chairul")
+            }
+            .clipShape(RoundedRectangle(cornerRadius: 26))
+            .padding(.bottom, 16)
+            
+            HStack {
+                // end call
+                Button {
+                    
+                } label: {
+                    Image(systemName: "phone.down.fill")
+                        .font(.system(size: 24))
+                        .padding(24)
+                }
+                .glassEffect()
+                .buttonBorderShape(.circle)
+                
+                // mute/unmute
+                Button {
+                    
+                } label: {
+                    Image(systemName: "microphone.fill")
+                        .font(.system(size: 36))
+                        .padding(36)
+                }
+                .glassEffect()
+                .buttonBorderShape(.circle)
+                
+                // leave call
+                Button {
+                    
+                } label: {
+                    Image(systemName: "door.left.hand.open")
+                        .font(.system(size: 24))
+                        .padding(24)
+                }
+                .glassEffect()
+                .buttonBorderShape(.circle)
+            }
         }
+        .padding(16)
     }
 }
 
