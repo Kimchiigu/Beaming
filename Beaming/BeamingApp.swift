305c9951a3f0bfec43952d9caf7671f4310aac7e
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2026-07-02 08:21:12 +0700

    Initial Commit

---
+++ Beaming/BeamingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeamingApp.swift
+//  Beaming
+//
+//  Created by Christopher Hardy Gunawan on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BeamingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
